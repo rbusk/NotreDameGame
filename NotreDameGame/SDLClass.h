f2d58d61dbd56a795307463946c4e83d05315895
Mary Connolly <mconnol6@nd.edu>
2015-03-24 17:49:53 -0400

    Removed XCode project

---
+++ NotreDameGame/SDLClass.h
@@ -0,0 +1,14 @@
+//
+//  SDLClass.h
+//  NotreDameGame
+//
+//  Created by Mary Connolly on 3/23/15.
+//  Copyright (c) 2015 Mary. All rights reserved.
+//
+
+#ifndef __NotreDameGame__SDLClass__
+#define __NotreDameGame__SDLClass__
+
+#include <stdio.h>
+
+#endif /* defined(__NotreDameGame__SDLClass__) */
